0890bc6b0b3f0fc256f0e9d1952293c1d373a8f7
Mobin Asri <masri@ucsc.edu>
2021-06-19 22:03:11 +0000

    Added two tasks for fitting model to the male and female coverage dists

--- asset/docker/asset/scripts/fit_model_extra.py
+++ asset/docker/asset/scripts/fit_model_extra.py
@@ -30,7 +30,7 @@ def main():
     Y = [scale_factor * y for y in Y]
     # print the probability table
     with open(outputPath,"w+") as f:
-        f.write("#coverage\tfreq\tfitted\terror\textra\thaploid\tcollapsed\n")
+        f.write("#coverage\tfreq\tfit\terror\textra\thaploid\tcollapsed\n")
         for cov in range(len(counts)):
             prob_err = cov_model.probability_erroneous(cov)
             prob_extra = cov_model.probability_extra(cov)
